57d711ea0c048c28b6715fb07cda06f405f70fc7
Sumanth D <sumanthd032@gmail.com>
2026-04-23 22:59:14 +0530

    feat: project setup, hello world controller and index page

---
+++ src/test/java/com/todorails/TodoRailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todorails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoRailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
